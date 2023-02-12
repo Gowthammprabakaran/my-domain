46c62f985178221f0a873769d10aa78391076e45
Gowthammprabakaran <gowthammprabakaran@gmail.com>
2023-02-12 20:41:35 +0530

    Gold Coin

---
+++ src/org/java1/Furious.java
@@ -0,0 +1,14 @@
+package org.java1;
+
+public class Furious {
+
+	public void koko() {
+		System.out.println("gowthamm");
+	}
+	
+	public static void main(String[] args) {
+		Furious fur = new Furious();
+		fur.koko();
+	}
+
+}
